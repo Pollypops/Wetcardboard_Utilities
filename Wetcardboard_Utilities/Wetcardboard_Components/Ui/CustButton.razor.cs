999cb7f75163a8b31617723d4c00b41eeac7df88
Pollypops <103785024+Pollypops@users.noreply.github.com>
2023-12-31 01:45:28 +0100

    Add ChildContent to CustButton

---
+++ Wetcardboard_Utilities/Wetcardboard_Components/Ui/CustButton.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Wetcardboard_Components.Ui
+{
+    public partial class CustButton
+    {
+        [Parameter]
+        public RenderFragment? ChildContent { get; set; }
+    }
+}
